19cd71edaaf6196313eda66c04c2876a06fe3559
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2024-12-23 12:00:04 -0300

    Continuando next-crud

---
+++ Next/next-crud/src/components/Botao.tsx
@@ -0,0 +1,24 @@
+interface BotaoProps {
+    cor?: 'green' | 'blue' | 'gray'
+    className?: string
+    children: any
+}
+
+export default function Botao(props: BotaoProps) {
+    const cor = props.cor ?? 'gray'
+    const corClasse = {
+        green: 'from-green-400 to-green-700',
+        blue: 'from-blue-400 to-blue-700',
+        gray: 'from-gray-400 to-gray-700',
+    }[cor];
+    
+    return (
+        <button className={`
+            bg-gradient-to-r ${corClasse}
+            text-white px-4 py-2 rounded-md
+            ${props.className}
+        `}>
+            {props.children}
+        </button>
+    )
+}
